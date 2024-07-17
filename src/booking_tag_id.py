2a65f04e4ad1a3412fd2e3f6fd7843e82a11343c
fistaco <fiske_schijlen@hotmail.com>
2024-07-17 22:12:05 +0200

    Represent tagIds of popular bookable products with an enum.

---
+++ src/booking_tag_id.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class BookingTagId(Enum):
+    STRENGTH = 5
+    GYM = 22
+    BEACH_VOLLEYBALL_COURT = 88
+    BODY_POWER = 121
+    HALL_X1 = 147
+    HALL_X2 = 148
+    HALL_X3 = 149
+    POWER_KICK = 161
