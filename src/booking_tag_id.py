72d4c0ca912f53f1b16f57f4b304944cb1a539f4
fistaco <fiske_schijlen@hotmail.com>
2024-07-19 21:21:23 +0200

    Implement option to construct a booking tag ID from a string by fuzzy searching.

--- src/booking_tag_id.py
+++ src/booking_tag_id.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from fuzzywuzzy import process
+
 
 class BookingTagId(Enum):
     STRENGTH = 5
@@ -10,3 +12,28 @@ class BookingTagId(Enum):
     HALL_X2 = 148
     HALL_X3 = 149
     POWER_KICK = 161
+
+    def __str__(self) -> str:
+        """Represents this booking tag ID as a string by """
+        return self.name.replace("_", " ").capitalize()
+
+    @staticmethod
+    def from_string(s):
+        """
+        Constructs and returns a booking tag ID from the given category string
+        `s` by fuzzy searching for the best match.
+
+        >>> BookingTagId.from_string("gYm")
+        <BookingTagId.GYM: 28>
+        >>> BookingTagId.from_string("beach")
+        <BookingTagId.BEACH_VOLLEYBALL_COURT: 88>
+        """
+        match, _ = process.extractOne(s, BOOKING_TAG_ID_STRING_MAP.keys())
+        return BOOKING_TAG_ID_STRING_MAP[match]
+
+
+BOOKING_TAG_ID_STRING_MAP = {
+    str(booking_tag_id): booking_tag_id
+    for booking_tag_id
+    in list(BookingTagId)
+}
